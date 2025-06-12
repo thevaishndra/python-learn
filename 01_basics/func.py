b704f99a8a23302f709edaa359fcedd52f197078
thevaishndra <chandrav306m@gmail.com>
2025-06-12 17:23:34 +0530

    intro + datattypes

---
+++ 01_basics/func.py
@@ -0,0 +1,9 @@
+from hello_world import func
+
+func(10)
+
+# this __pycache__is created automatically to store bytecode files
+# works only for imported files, not for top level files
+# python virtual machine -> code loop to iterate byte code
+# run time engine also known as python interpreter
+# byte code is not machine code
---
+++ 01_basics/hello_world.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+def func(n):
+    print(n)
+
+func(4)
+
+# object types / datatypes
+# number : 1234, 3.14, 3+4j, 0b111, decimal(), fraction()
+# string : 'spam', "bob's", b'a\x01c', u'sp\xc4m'
+# list : [1, [2, 'three'], 4.5], list(Range(10))
+# tuple : (1, 'spam', 4, 'U'), tuple('spam'), namedtuple
+# Dictionary : {'food': 'spam', 'taste': 'yum'}, (hours=10)
+# set : set('abc'), {'a', 'b', 'c'}
+# File : open('eggs.txt'), open('eggs.txt', 'w')
+# Boolean : True, False
+# None : None
+# Function, modules, classes, instances, exceptions
+# advance: Decorators, generators, iterators, metaprogramming
